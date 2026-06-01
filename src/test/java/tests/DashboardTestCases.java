79b0fb65a1b04ba66996213e5fa12c82a04fcab2
Ilja <ilja.markov.93@gmail.com>
2026-06-01 19:54:26 +0300

    skeleton-card check

--- src/test/java/tests/DashboardTestCases.java
+++ src/test/java/tests/DashboardTestCases.java
@@ -21,12 +21,8 @@ public class DashboardTestCases extends BaseTest {
         loginPage.loginAdmin("admin", "admin123");
         open("/dashboard");
         $("#dashboard-page-container").shouldHave(attribute("data-loading", "true"));
-        $("[data-testid='skeleton-card']").shouldHave(CollectionCondition.sizeGreaterThan(0));
-
-
+        $$("[data-testid='skeleton-card']").forEach(card -> card.shouldBe(visible));
 
     }
-
-
 
 }
